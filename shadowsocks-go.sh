3f4a011e1743cd8a56116d95a84c373e773e341c
AMebius <wjk19951114@gmail.com>
2019-05-14 22:14:43 +0800

    change ss-go source

--- shadowsocks-go.sh
+++ shadowsocks-go.sh
@@ -230,7 +230,7 @@ pre_install(){
 download_files(){
     cd ${cur_dir}
     if is_64bit; then
-        if ! wget --no-check-certificate -c https://dl.lamp.sh/shadowsocks/shadowsocks-server-linux64-1.2.2.gz; then
+        if ! wget --no-check-certificate -c https://raw.githubusercontent.com/wjk199511140034/ss-onekeyinstall/master/ss-go/shadowsocks-server-linux64-1.2.2.gz; then
             echo -e "[${red}Error${plain}] Failed to download shadowsocks-server-linux64-1.2.2.gz"
             exit 1
         fi
@@ -241,7 +241,7 @@ download_files(){
         fi
         mv -f shadowsocks-server-linux64-1.2.2 /usr/bin/shadowsocks-server
     else
-        if ! wget --no-check-certificate -c https://dl.lamp.sh/shadowsocks/shadowsocks-server-linux32-1.2.2.gz; then
+        if ! wget --no-check-certificate -c https://raw.githubusercontent.com/wjk199511140034/ss-onekeyinstall/master/ss-go/shadowsocks-server-linux32-1.2.2.gz; then
             echo -e "[${red}Error${plain}] Failed to download shadowsocks-server-linux32-1.2.2.gz"
             exit 1
         fi
